945eba571af6d698b4e40a0cdd4cb2eb3b8e9c6f
jonhenry63 <eeu489@bangor.ac.uk>
2018-04-30 22:52:09 +0100

    Added in some jedis functionality(tried). Getting error: "Cannot resolve method close()".

--- app/actors/TemperatureActor.java
+++ app/actors/TemperatureActor.java
@@ -2,9 +2,14 @@ package actors;
 
 import akka.actor.AbstractActor;
 import akka.actor.Props;
+import redis.clients.jedis.*;
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,6 +34,19 @@ public class TemperatureActor extends AbstractActor {
                 .match(Long.class, tmp ->{
                     temps.add(tmp);
                 })
+                .match(RecieveTemperature.class, p -> {
+                    Jedis jedis = null;
+                    try {
+                        jedis = pool.getResource();
+                        jedis.zadd(p.getSensorID().toString(),p.getTimeStampLong(), p.getTemp().toString());
+
+                    } finally {
+                        if (jedis != null) {
+                            jedis.close();
+                        }
+                    }
+                    pool.close();
+                        })
                 .matchEquals("latest", p -> {
                     switch (temps.size()) {
                         case 0:
@@ -40,6 +58,45 @@ public class TemperatureActor extends AbstractActor {
                     }
                 })
                 .build();
+    }
+
+    public static final class RecieveTemperature{
+        final Float temp;
+        final Long sensorID;
+        final LocalDateTime timeStamp;
+
+        public RecieveTemperature(Float temp, Long sensorID, LocalDateTime timeStamp) {
+            this.temp = temp;
+            this.sensorID = sensorID;
+            this.timeStamp = timeStamp;
+        }
+
+        public RecieveTemperature(Float temp, Long sensorID, String timeStamp) {
+            this.temp = temp;
+            this.sensorID = sensorID;
+
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-ddTHH:mm:ssZ");
+            LocalDateTime dateTime = LocalDateTime.parse(timeStamp, formatter);
+            this.timeStamp = dateTime;
+        }
+
+        public Float getTemp() {
+            return temp;
+        }
+
+        public LocalDateTime getTimeStamp() {
+            return timeStamp;
+        }
+
+        public Long getTimeStampLong(){
+            Long epoch = timeStamp.atZone(ZoneId.systemDefault()).toEpochSecond();
+            return epoch;
+        }
+
+
+        public Long getSensorID() {
+            return sensorID;
+        }
     }
 }
 
